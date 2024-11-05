1d4849fa5b9eb14af5700edee55f7b5bbb8be0f7
zcruz6590 <zoenoel_cruz@Yahoo.com>
2024-11-05 04:17:55 -0800

    redid html, css, and js. Produced a calculator using class object in Javascript

--- script.js
+++ script.js
@@ -1,23 +1,112 @@
-// Select all elements with class "number"
-const number = document.querySelectorAll(".number");
+class Calculator {
+    constructor(previousOperandElement, currentOperandElement) {
+        this.previousOperandElement = previousOperandElement
+        this.currentOperandElement = currentOperandElement
+        this.clear()
+    }
 
-// Select the screen input field
-const screen = document.querySelector(".screen input");
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operator = null
+    }
 
-// Select input element with the value of "AC"
-const AC = document.querySelector('input[value="AC"]');
+    toggleSign() {
+        this.currentOperand = this.currentOperand * -1
+    }
 
-// Select 
+    appendNumber(number) {
+        if(number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = (this.currentOperand.toString() || '') + number.toString();
+    }
 
-// Clear the screen
-AC.onclick = () => {
-    screen.value = '';
-};
+    chooseOperator(operator) {
+        if(this.currentOperand === '') return
+        if(this.previousOperand !== '') {
+            this.compute()
+        }
+        this.operator = operator
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+    }
 
-//Loop over each number button
+    compute() {
+        let computation = 0
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operator) {
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case 'x':
+            computation = prev * current
+                break
+            case '÷':
+            computation = prev / current
+                break
+            case '%':
+            computation = prev % current
+                break
+            default: 
+                return
+        }
+        this.currentOperand = computation
+        this.operator = null
+        this.previousOperand = ''
+    }
+
+    updateDisplay() {
+        this.currentOperandElement.innerText = this.currentOperand;
+        this.previousOperandElement.innerText = this.previousOperand;
+    }
+}
+
+// Get all variable buttons on the calculator
+const number = document.querySelectorAll('.number');
+const AC = document.querySelector('.AC');
+const operator = document.querySelectorAll('.operator')
+const toggleSign = document.querySelector('.pos-neg');
+const decimal = document.querySelector('.decimal');
+const equals = document.querySelector('.equals');
+const previousOperandElement = document.querySelector('.previous-operand');
+const currentOperandElement = document.querySelector('.current-operand');
+
+const calculator = new Calculator(previousOperandElement, 
+    currentOperandElement)
+
+toggleSign.addEventListener('click', () => {
+    calculator.toggleSign()
+    calculator.updateDisplay()
+})
+
 number.forEach(button => {
-    button.onclick = () => {
-        screen.value += button.value;
-    };
-});
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
 
+AC.addEventListener('click', () => {
+    calculator.clear()
+})
+
+operator.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperator(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equals.addEventListener('click', () => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+AC.addEventListener('click', () => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
